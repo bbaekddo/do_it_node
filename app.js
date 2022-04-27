3b3b5f6ca480763328404d4d44055c49f76d804c
bbaekddo <bbaekddo100@naver.com>
2022-04-27 22:20:26 +0900

    요청화면별 라우팅 함수 추가

--- app.js
+++ app.js
@@ -150,6 +150,70 @@ database.init(app, config);
 // 라우팅 시작
 routeLoader.init(app, router);
 
+// 홈 화면 라우팅
+router.route('/').get((req, res) => {
+    console.log('/ 패스 요청됨');
+    
+    res.render('index.ejs');
+});
+
+// 로그인 요청 화면
+app.get('/login', (req, res) => {
+    console.log('/login 패스 요청됨');
+    
+    res.render('login.ejs', { message: req.flash('loginMessage') });
+});
+
+app.post('/login', passport.authenticate('localLogin', {
+    successRedirect: '/profile',
+    failureRedirect: '/login',
+    failureFlash: true
+}));
+
+// 회원가입 요청 화면
+app.get('/signUp', (req, res) => {
+    console.log('/signUp 패스 요청됨');
+    
+    res.render('signUp.ejs', { message: req.flash('signUpMessage') });
+});
+
+app.post('/signUp', passport.authenticate('localSignUp', {
+    successRedirect: '/profile',
+    failureRedirect: '/signUp',
+    failureFlash: true
+}));
+
+// 프로필 요청 화면
+router.route('/profile').get((req, res) => {
+    console.log('/profile 패스 요청됨');
+    
+    // 인증된 경우 req.user 객체에 사용자 정보가 있으며, 인증이 안된 경우 req.user는 false
+    console.log('req.user 객체의 값');
+    console.dir(req.user);
+    
+    // 인증이 안된 경우
+    if (!req.user) {
+        console.log('사용자 인증이 안된 상태');
+        res.redirect('/');
+        return;
+    }
+    
+    // 인증된 경우
+    console.log('사용자 인증 된 상태');
+    if (Array.isArray(req.user)) {
+        res.render('profile.ejs', { user: req.user[0]._doc });
+    } else {
+        res.render('profile.ejs', { user: req.user });
+    }
+});
+
+// 로그아웃 요청 화면
+app.get('/logout', (req, res) => {
+    console.log('/logout 패스 요청됨');
+    req.logout();
+    res.redirect('/');
+});
+
 /*
 // 404 오류 페이지 설정
 const errorHandler = expressErrorHandler({
