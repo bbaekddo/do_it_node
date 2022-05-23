ea8b68c9563a5d53987679753d32301d9d27d0b0
bbaekddo <bbaekddo100@naver.com>
2022-05-23 14:43:28 +0900

    echo error 함수 테스트

--- handler/handlerInfo.js
+++ handler/handlerInfo.js
@@ -1,7 +1,8 @@
 console.log('handler info 파일 로딩됨');
 
 const handlerInfo = [
-    { file: `./echo`, method: 'echo' }
+    { file: `./echo`, method: 'echo' },
+    { file: './echoError', method: 'echoError'}
 ];
 
 module.exports = handlerInfo;
