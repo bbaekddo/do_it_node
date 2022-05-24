78b9ba127140f8c1dc4cd0ea0b9a5dc477117dd4
bbaekddo <bbaekddo100@naver.com>
2022-05-24 10:23:54 +0900

    add 함수 테스트

--- handler/handlerInfo.js
+++ handler/handlerInfo.js
@@ -2,7 +2,8 @@ console.log('handler info 파일 로딩됨');
 
 const handlerInfo = [
     { file: `./echo`, method: 'echo' },
-    { file: './echoError', method: 'echoError'}
+    { file: './echoError', method: 'echoError'},
+    { file: './add', method: 'add'}
 ];
 
 module.exports = handlerInfo;
